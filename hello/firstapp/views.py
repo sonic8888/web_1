91b667a0e1416be314aec394d34ab5d066a9c1cc
sonic <sonic45600@gmail.com>
2023-03-11 21:14:08 +0300

    11.03

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.template.response import TemplateResponse
 
 
 def index(request):
-    return render(request, "index.html")
+    # header = "Персональные данные"  # обычная переменная
+    # langs = ["Английский", "Немецкий", "Испанский"]  # массив
+    # user = {"name": "Максим,", "age": 30}  # словарь
+    # addr = ("Виноградная", 23, 45)  # кортеж
+    # data = {"header": header, "langs": langs, "user": user, "address": addr}
+    return TemplateResponse(request, "firstapp\\home.html")
 
 
 def about(request):
